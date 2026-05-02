0e9ba170062571d227402fc95b3933d71717cf42
Vincent <v23zhou@uwaterloo.ca>
2026-05-02 01:40:46 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Classroom backend is up.' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}/`);
+});
